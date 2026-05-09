59bcd9877743eb794a90d6c1b1d7c248896c4ecc
DhandyF <dhandy.friyadi@gmail.com>
2026-05-09 08:04:26 +0700

    responsive table width

--- src/views/pre-order/PreOrdersView.vue
+++ src/views/pre-order/PreOrdersView.vue
@@ -316,7 +316,7 @@ async function handleDelete() {
             </div>
           </template>
           <template #expanded="{ row }">
-            <table class="w-[85vw] mx-auto text-sm">
+            <table class="w-14/15 mx-auto text-sm">
               <thead>
                 <tr class="border-b border-surface-200">
                   <th class="py-1.5 px-3 text-left font-medium text-surface-500">Size</th>
